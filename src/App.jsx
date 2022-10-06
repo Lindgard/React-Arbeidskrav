0e92fc7674226eea55167e3ca242895bef6127f3
Lindgard <a.b.lindgard@gmail.com>
2022-10-06 13:50:03 +0200

    prepared player and dealer components to be written

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import { cardData } from "./components/deckOfCards.js";
+// import { useState, useEffect } from "react";
+// import { cardData } from "./components/DeckOfCards.js";
 
 const App = () => {
+  // cardData;
+
   return <div className="App"></div>;
 };
 
